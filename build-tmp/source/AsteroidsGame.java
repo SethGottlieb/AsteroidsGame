6479fa1f1f2948e11f088acc8862f731943a8c79
unknown <segottlieb@697-334-Sauron.sf.k12.internal>
2014-11-05 10:32:47 -0800

    Created program, combined with Starfield, updated index.html

--- build-tmp/source/AsteroidsGame.java
+++ build-tmp/source/AsteroidsGame.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class AsteroidsGame extends PApplet {
+
 boolean UpIsPressed = false;
 boolean LeftIsPressed = false;
 boolean RightIsPressed = false;
@@ -27,7 +43,7 @@ public void draw()
 	Normandy.show();
 	if(UpIsPressed == true) //Up
 	{
-		Normandy.accelerate(0.1);
+		Normandy.accelerate(0.1f);
 	}
 	if(LeftIsPressed == true) //Left
 	{
@@ -221,4 +237,13 @@ abstract class Floater //Do NOT modify the Floater class! Make changes in the Sp
 		}   
 		endShape(CLOSE);  
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "AsteroidsGame" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
